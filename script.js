c999b8f82aa22f8f63d5cbf24609d4381f1486fe
Saurabh Wankhade <drsaurabh2018@gmail.com>
2024-02-29 13:17:14 +0530

    On Sign Up and login page password visible added, closed login page and sign up added with animation

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const outterDiv = document.querySelector('.outerDiv');
 
+const innerDiv = document.querySelector('.innerDiv');
+
+let checkEye = true;
+
 
 
 // on click password will visible
 
-function passwordVisible(){
-const eyeBtn = document.querySelector('.eye');
-let checkEye = true;
-eyeBtn.addEventListener('click',()=>{
-    const password = document.getElementById('password');
+function passwordVisible(eyeName,passName){
+const eyeBtn = document.querySelector(`.${eyeName}`);
+
+    const password = document.getElementById(`${passName}`);
     if(checkEye){
         password.setAttribute('type','text');
         eyeBtn.classList.remove('fa-eye-slash');
@@ -20,7 +23,6 @@ eyeBtn.addEventListener('click',()=>{
         eyeBtn.classList.add('fa-eye-slash');
         checkEye = true;
     }
-});
 }
 
 
@@ -28,35 +30,12 @@ eyeBtn.addEventListener('click',()=>{
 // on click login btn
 function showLoginPage(){
 const login_btn = document.querySelector('.login_btn');
-const innerDiv = document.querySelector('.innerDiv');
 
 login_btn.addEventListener('click',()=>{
     outterDiv.classList.add('opacityStart');
     outterDiv.style.visibility = 'visible';
 
-    innerDiv.innerHTML = ` <span><i class="fas fa-times closed"></i></span>
-    <h4>Login</h4>
-    <form>
-        <div> <span><i class="far fa-envelope"></i></span><input type="email"  id="userEmail" placeholder="Enter you email" ></div>
-        <div>
-            <span><i class="fa-solid fa-lock"></i></span>
-            <input type="password"  id="password" placeholder="Enter your password">
-            <span>
-                <i class="fa-solid fa-eye-slash eye"></i>
-            </span>
-        </div>
-        <div class="forgot_div">
-            <p>Forgot Password</p>
-        </div>
-        <div class="btn">
-            <input type="button" value="Login">
-        </div>
-        <div class="signup_text">
-            <p>Don&#8217;t have an account?  <span class="signup_btn" onclick="signupBtn()">&nbsp;Signup</span></p>
-        </div>
-    </form>
-
-    `;
+   setInnerLoginPage();
 
     closedDilogBox();
     
@@ -71,7 +50,34 @@ login_btn.addEventListener('click',()=>{
 showLoginPage();
 
 
+function setInnerLoginPage(){
 
+    innerDiv.innerHTML = ` <span><i class="fas fa-times closed"></i></span>
+    <h4>Login</h4>
+    <form>
+        <div> <span><i class="far fa-envelope"></i></span><input type="email"  id="userEmail" placeholder="Enter you email" required></div>
+        <div>
+            <span><i class="fa-solid fa-lock" ></i></span>
+            <input type="password"  id="password" placeholder="Enter your password" required>
+            <span>
+                <i class="fa-solid fa-eye-slash eye" onclick=" passwordVisible('eye','password')"></i>
+            </span>
+        </div>
+        <div class="forgot_div">
+            <p>Forgot Password</p>
+        </div>
+        <div class="btn">
+            <input type="button" value="Login" onclick="loginClick()">
+        </div>
+        <div class="signup_text">
+            <p>Don&#8217;t have an account?  <span class="signup_btn" onclick="signupBtn()">&nbsp;Signup</span></p>
+        </div>
+    </form>
+
+    `;
+
+    closedDilogBox();
+}
 
 // on click closed btn 
 function closedDilogBox(){
@@ -100,35 +106,50 @@ function signupBtn(){
     <span><i class="fas fa-times closed"></i></span>
     <h4>Sign Up</h4>
     <form>
-        <div> <span><i class="far fa-envelope"></i></span><input type="email"  id="userEmail" placeholder="Enter you email" ></div>
+        <div> <span><i class="far fa-envelope"></i></span><input type="email"  id="userEmail" placeholder="Enter you email" required></div>
         <div>
             <span><i class="fa-solid fa-lock"></i></span>
-            <input type="password"  id="createPass" placeholder="Create password">
+            <input type="password"  id="createPass" placeholder="Create password" required>
             <span>
-                <i class="fa-solid fa-eye-slash eye"></i>
+                <i class="fa-solid fa-eye-slash eye createeye" onclick=" passwordVisible('createeye','createPass')"></i>
             </span>
         </div>
         <div>
             <span><i class="fa-solid fa-lock"></i></span>
-            <input type="password"  id="comfirmPass" placeholder="Confirm password">
+            <input type="password"  id="comfirmPass" placeholder="Confirm password" required>
             <span>
-                <i class="fa-solid fa-eye-slash eye"></i>
+                <i class="fa-solid fa-eye-slash eye confirEye" onclick=" passwordVisible('confirEye','comfirmPass')"></i>
             </span>
         </div>
 
         <div class="btn" id="signUp">
-            <input type="button" value="Sign Up">
+            <input type="button" value="Sign Up" onclick="loginClick()">
         </div>
         <div class="login_text">
-            <p>Alreay have a account?  <span class="btn_login" onclick="showLoginPage()">&nbsp;Login</span></p>
+            <p>Alreay have a account?  <span class="btn_login" onclick="setInnerLoginPage()">&nbsp;Login</span></p>
         </div>
     </form>
 
     `;
 
     closedDilogBox();
-    passwordVisible();
+   
 
+}
+
+
+function loginClick(){
+    const uEmail = document.getElementById('userEmail').value;
+    const uPass = document.getElementById('password').value;
+    if(uEmail === ''){
+        console.log('User email empty!');
+    } else if(uPass === ''){
+        console.log('User password empty!');
+    } else if(uPass.length < 7){
+        console.log('password less than 7!');
+    } else{
+        console.log(`UserEmail : ${uEmail} nad UserPass : ${uPass}`);
+    }
 }
 
 
